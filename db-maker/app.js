735bb7ff8d1e4e6c609959efbd547caa9ea051a0
sashase <semenchuksasha21@gmail.com>
2023-01-20 20:44:26 +0200

    1st-year-added-to-db

--- db-maker/app.js
+++ db-maker/app.js
@@ -9,201 +9,354 @@ XLSX.set_fs(fs)
 XLSX.stream.set_readable(Readable)
 XLSX.set_cptable(cpexcel)
 
-const file = XLSX.readFile("./file.xlsx")
+const file1 = XLSX.readFile("./file1.xlsx")
+const file2 = XLSX.readFile("./file2.xlsx")
 
-const workbook1 = file.Sheets[file.SheetNames[0]]
-const workbook2 = file.Sheets[file.SheetNames[1]]
+const workbooks = [
+  [file1.Sheets[file1.SheetNames[0]], file1.Sheets[file1.SheetNames[1]]],
+  [file2.Sheets[file2.SheetNames[0]], file2.Sheets[file2.SheetNames[1]]]
+]
 
-const groups = ["kp21", "kp22", "ksr21", "kt21", "km21", "ipz21", "kn21"]
+const groups = [
+  ["group1", "group2", "group3", "group4", "group5", "group6", "group7"],
+  ["kp21", "kp22", "ksr21", "kt21", "km21", "ipz21", "kn21"]
+]
 
-let schedule1 = {
-  kp21: {
-    monday: [],
-    tuesday: [],
-    wednesday: [],
-    thursday: [],
-    friday: []
-  },
-  kp22: {
-    monday: [],
-    tuesday: [],
-    wednesday: [],
-    thursday: [],
-    friday: []
-  },
-  ksr21: {
-    monday: [],
-    tuesday: [],
-    wednesday: [],
-    thursday: [],
-    friday: []
-  },
-  kt21: {
-    monday: [],
-    tuesday: [],
-    wednesday: [],
-    thursday: [],
-    friday: []
-  },
-  km21: {
-    monday: [],
-    tuesday: [],
-    wednesday: [],
-    thursday: [],
-    friday: []
-  },
-  ipz21: {
-    monday: [],
-    tuesday: [],
-    wednesday: [],
-    thursday: [],
-    friday: []
-  },
-  kn21: {
-    monday: [],
-    tuesday: [],
-    wednesday: [],
-    thursday: [],
-    friday: []
-  }
-}
+const days = [
+  "monday",
+  "tuesday",
+  "wednesday",
+  "thursday",
+  "friday",
+  "saturday",
+  "sunday"
+]
 
-let schedule2 = {
-  kp21: {
-    monday: [],
-    tuesday: [],
-    wednesday: [],
-    thursday: [],
-    friday: []
+let year1 = {
+  schedule1: {
+    group1: {
+      monday: [],
+      tuesday: [],
+      wednesday: [],
+      thursday: [],
+      friday: []
+    },
+    group2: {
+      monday: [],
+      tuesday: [],
+      wednesday: [],
+      thursday: [],
+      friday: []
+    },
+    group3: {
+      monday: [],
+      tuesday: [],
+      wednesday: [],
+      thursday: [],
+      friday: []
+    },
+    group4: {
+      monday: [],
+      tuesday: [],
+      wednesday: [],
+      thursday: [],
+      friday: []
+    },
+    group5: {
+      monday: [],
+      tuesday: [],
+      wednesday: [],
+      thursday: [],
+      friday: []
+    },
+    group6: {
+      monday: [],
+      tuesday: [],
+      wednesday: [],
+      thursday: [],
+      friday: []
+    },
+    group7: {
+      monday: [],
+      tuesday: [],
+      wednesday: [],
+      thursday: [],
+      friday: []
+    }
   },
-  kp22: {
-    monday: [],
-    tuesday: [],
-    wednesday: [],
-    thursday: [],
-    friday: []
-  },
-  ksr21: {
-    monday: [],
-    tuesday: [],
-    wednesday: [],
-    thursday: [],
-    friday: []
-  },
-  kt21: {
-    monday: [],
-    tuesday: [],
-    wednesday: [],
-    thursday: [],
-    friday: []
-  },
-  km21: {
-    monday: [],
-    tuesday: [],
-    wednesday: [],
-    thursday: [],
-    friday: []
-  },
-  ipz21: {
-    monday: [],
-    tuesday: [],
-    wednesday: [],
-    thursday: [],
-    friday: []
-  },
-  kn21: {
-    monday: [],
-    tuesday: [],
-    wednesday: [],
-    thursday: [],
-    friday: []
-  }
-}
-
-const startingRows = [2, 8, 14, 20, 26]
-
-const startingColumns = {
-  kp21: {
-    lesson: "A",
-    teacher: "B",
-    classRoom: "C"
-  },
-  kp22: {
-    lesson: "D",
-    teacher: "E",
-    classRoom: "F"
-  },
-  ksr21: {
-    lesson: "G",
-    teacher: "H",
-    classRoom: "I"
-  },
-  kt21: {
-    lesson: "J",
-    teacher: "K",
-    classRoom: "L"
-  },
-  km21: {
-    lesson: "M",
-    teacher: "N",
-    classRoom: "O"
-  },
-  ipz21: {
-    lesson: "P",
-    teacher: "Q",
-    classRoom: "R"
-  },
-  kn21: {
-    lesson: "S",
-    teacher: "T",
-    classRoom: "U"
-  }
-}
-
-const addToDb = (group) => {
-  for (let i = 0; i < 5; i++) {
-    for (let j = 0; j < 6; j++) {
-      const lesson1 =
-        workbook1[`${startingColumns[group].lesson}${startingRows[i] + j}`]?.v
-      const teacher1 =
-        workbook1[`${startingColumns[group].teacher}${startingRows[i] + j}`]?.v
-      const classRoom1 =
-        workbook1[`${startingColumns[group].classRoom}${startingRows[i] + j}`]
-          ?.v
-
-      const lesson2 =
-        workbook2[`${startingColumns[group].lesson}${startingRows[i] + j}`]?.v
-      const teacher2 =
-        workbook2[`${startingColumns[group].teacher}${startingRows[i] + j}`]?.v
-      const classRoom2 =
-        workbook2[`${startingColumns[group].classRoom}${startingRows[i] + j}`]
-          ?.v
-
-      schedule1[group][Object.keys(schedule1[group])[i]].push({
-        lesson: lesson1 ? lesson1 : "",
-        teacher: teacher1 ? teacher1 : "",
-        classRoom: classRoom1 ? classRoom1 : ""
-      })
-
-      schedule2[group][Object.keys(schedule2[group])[i]].push({
-        lesson: lesson2 ? lesson2 : "",
-        teacher: teacher2 ? teacher2 : "",
-        classRoom: classRoom2 ? classRoom2 : ""
-      })
+  schedule2: {
+    group1: {
+      monday: [],
+      tuesday: [],
+      wednesday: [],
+      thursday: [],
+      friday: []
+    },
+    group2: {
+      monday: [],
+      tuesday: [],
+      wednesday: [],
+      thursday: [],
+      friday: []
+    },
+    group3: {
+      monday: [],
+      tuesday: [],
+      wednesday: [],
+      thursday: [],
+      friday: []
+    },
+    group4: {
+      monday: [],
+      tuesday: [],
+      wednesday: [],
+      thursday: [],
+      friday: []
+    },
+    group5: {
+      monday: [],
+      tuesday: [],
+      wednesday: [],
+      thursday: [],
+      friday: []
+    },
+    group6: {
+      monday: [],
+      tuesday: [],
+      wednesday: [],
+      thursday: [],
+      friday: []
+    },
+    group7: {
+      monday: [],
+      tuesday: [],
+      wednesday: [],
+      thursday: [],
+      friday: []
     }
   }
 }
 
-const uploadDb = async (schedule1, schedule2, docName) => {
-  await setDoc(doc(db, "schedule1", docName), {
+let year2 = {
+  schedule1: {
+    kp21: {
+      monday: [],
+      tuesday: [],
+      wednesday: [],
+      thursday: [],
+      friday: []
+    },
+    kp22: {
+      monday: [],
+      tuesday: [],
+      wednesday: [],
+      thursday: [],
+      friday: []
+    },
+    ksr21: {
+      monday: [],
+      tuesday: [],
+      wednesday: [],
+      thursday: [],
+      friday: []
+    },
+    kt21: {
+      monday: [],
+      tuesday: [],
+      wednesday: [],
+      thursday: [],
+      friday: []
+    },
+    km21: {
+      monday: [],
+      tuesday: [],
+      wednesday: [],
+      thursday: [],
+      friday: []
+    },
+    ipz21: {
+      monday: [],
+      tuesday: [],
+      wednesday: [],
+      thursday: [],
+      friday: []
+    },
+    kn21: {
+      monday: [],
+      tuesday: [],
+      wednesday: [],
+      thursday: [],
+      friday: []
+    }
+  },
+  schedule2: {
+    kp21: {
+      monday: [],
+      tuesday: [],
+      wednesday: [],
+      thursday: [],
+      friday: []
+    },
+    kp22: {
+      monday: [],
+      tuesday: [],
+      wednesday: [],
+      thursday: [],
+      friday: []
+    },
+    ksr21: {
+      monday: [],
+      tuesday: [],
+      wednesday: [],
+      thursday: [],
+      friday: []
+    },
+    kt21: {
+      monday: [],
+      tuesday: [],
+      wednesday: [],
+      thursday: [],
+      friday: []
+    },
+    km21: {
+      monday: [],
+      tuesday: [],
+      wednesday: [],
+      thursday: [],
+      friday: []
+    },
+    ipz21: {
+      monday: [],
+      tuesday: [],
+      wednesday: [],
+      thursday: [],
+      friday: []
+    },
+    kn21: {
+      monday: [],
+      tuesday: [],
+      wednesday: [],
+      thursday: [],
+      friday: []
+    }
+  }
+}
+
+const startingRows = [
+  [2, 9, 16, 23, 30],
+  [2, 8, 14, 20, 26]
+]
+
+const startingColumns = [
+  {
+    lesson: "A",
+    teacher: "B",
+    classRoom: "C"
+  },
+  {
+    lesson: "D",
+    teacher: "E",
+    classRoom: "F"
+  },
+  {
+    lesson: "G",
+    teacher: "H",
+    classRoom: "I"
+  },
+  {
+    lesson: "J",
+    teacher: "K",
+    classRoom: "L"
+  },
+  {
+    lesson: "M",
+    teacher: "N",
+    classRoom: "O"
+  },
+  {
+    lesson: "P",
+    teacher: "Q",
+    classRoom: "R"
+  },
+  {
+    lesson: "S",
+    teacher: "T",
+    classRoom: "U"
+  }
+]
+
+const addToDb = (year, group, groupIndex) => {
+  for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < (year == 0 ? 7 : 6); j++) {
+      const lesson1 =
+        workbooks[year][0][
+          `${startingColumns[groupIndex].lesson}${startingRows[year][i] + j}`
+        ]?.v
+      const teacher1 =
+        workbooks[year][0][
+          `${startingColumns[groupIndex].teacher}${startingRows[year][i] + j}`
+        ]?.v
+      const classRoom1 =
+        workbooks[year][0][
+          `${startingColumns[groupIndex].classRoom}${startingRows[year][i] + j}`
+        ]?.v
+
+      const lesson2 =
+        workbooks[year][1][
+          `${startingColumns[groupIndex].lesson}${startingRows[year][i] + j}`
+        ]?.v
+      const teacher2 =
+        workbooks[year][1][
+          `${startingColumns[groupIndex].teacher}${startingRows[year][i] + j}`
+        ]?.v
+      const classRoom2 =
+        workbooks[year][1][
+          `${startingColumns[groupIndex].classRoom}${startingRows[year][i] + j}`
+        ]?.v
+
+      if (year == 0) {
+        year1.schedule1[group][Object.keys(year1.schedule1[group])[i]].push({
+          lesson: lesson1 ? lesson1 : "",
+          teacher: teacher1 ? teacher1 : "",
+          classRoom: classRoom1 ? classRoom1 : ""
+        })
+        year1.schedule2[group][Object.keys(year1.schedule2[group])[i]].push({
+          lesson: lesson2 ? lesson2 : "",
+          teacher: teacher2 ? teacher2 : "",
+          classRoom: classRoom2 ? classRoom2 : ""
+        })
+      } else {
+        year2.schedule1[group][Object.keys(year2.schedule1[group])[i]].push({
+          lesson: lesson1 ? lesson1 : "",
+          teacher: teacher1 ? teacher1 : "",
+          classRoom: classRoom1 ? classRoom1 : ""
+        })
+        year2.schedule2[group][Object.keys(year2.schedule2[group])[i]].push({
+          lesson: lesson2 ? lesson2 : "",
+          teacher: teacher2 ? teacher2 : "",
+          classRoom: classRoom2 ? classRoom2 : ""
+        })
+      }
+    }
+  }
+}
+
+const uploadDb = async (schedule1, schedule2, year) => {
+  await setDoc(doc(db, `year${year}`, "schedule1"), {
     ...schedule1
   })
-  await setDoc(doc(db, "schedule2", docName), {
+  await setDoc(doc(db, `year${year}`, "schedule2"), {
     ...schedule2
   })
 }
 
-for (let i = 0; i < groups.length; i++) {
-  addToDb(groups[i])
-  uploadDb(schedule1[groups[i]], schedule2[groups[i]], groups[i])
+for (let i = 0; i < 2; i++) {
+  for (let j = 0; j < 7; j++) {
+    addToDb(i, groups[i][j], j)
+  }
 }
+
+uploadDb(year1.schedule1, year1.schedule2, "1")
+uploadDb(year2.schedule1, year2.schedule2, "2")
+
+// !!TO REFACTOR!!
--- db-maker/utils/firebase.js
+++ db-maker/utils/firebase.js
@@ -1,12 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
-import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import * as dotenv from 'dotenv'
 dotenv.config()
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.PUBLIC_API_KEY,
   authDomain: process.env.PUBLIC_AUTH_DOMAIN,
@@ -15,15 +13,6 @@ const firebaseConfig = {
   messagingSenderId: process.env.PUBLIC_MESSAGING_SENDER_ID,
   appId: process.env.PUBLIC_APP_ID
 }
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyB_yajfK5mrIYR4a4Fne6MoMdJsZgfK5HE",
-//   authDomain: "schedule-app-2ae96.firebaseapp.com",
-//   projectId: "schedule-app-2ae96",
-//   storageBucket: "schedule-app-2ae96.appspot.com",
-//   messagingSenderId: "259952867065",
-//   appId: "1:259952867065:web:36a8997d3b5bd519fcd428"
-// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
